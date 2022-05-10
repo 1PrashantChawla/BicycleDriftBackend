6d13d90e66a13e3f710d941d72a9eb31c2e4179d
kartik18g <gupta.kartik18kg@gmail.com>
2022-05-10 23:08:20 +0530

    change

--- server.js
+++ server.js
@@ -31,16 +31,7 @@ app.use("/api", require("./routes/order"));
 app.use("/api", require("./routes/payment"));
 app.use("/api", require("./routes/admindata"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
+app.get('/',(req,res)=>res.send("server running"))
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT} `));
